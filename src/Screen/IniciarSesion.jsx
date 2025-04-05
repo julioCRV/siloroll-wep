bfe1994513893c67daef306f059e5f7adeccc32f
JulioCRV <julioroquevalda6@gmail.com>
2025-04-05 11:52:43 -0400

    Cambio en el inicio de sesión

--- src/Screen/IniciarSesion.jsx
+++ src/Screen/IniciarSesion.jsx
@@ -12,6 +12,47 @@ const Login = ({ login }) => {
   const [message, setMessage] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+
+  //   const data = {
+  //     user_name: user,
+  //     password: password,
+  //   };
+
+  //   try {
+  //     const response = await fetch("/api/user/login", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(data),
+  //     });
+
+  //     const result = await response.json();
+  //     console.log(result)
+
+  //     if (result.user_code === 1) {
+  //       localStorage.setItem('idusuario', result.user_code)
+  //       setOpenModal(true);
+  //       setMessage(true)
+  //       setTimeout(() => {
+  //         navigate('/Menu');
+  //         login(result.user_code);
+  //       }, 2400);
+  //     } else {
+  //       setOpenModal(true);
+  //       setMessage(false);
+  //       setTimeout(() => {
+  //         setOpenModal(false);
+  //       }, 1600);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error en la solicitud:", error);
+  //     setMessage("Hubo un problema al conectar con el servidor.");
+  //   }
+  // };
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
@@ -20,36 +61,20 @@ const Login = ({ login }) => {
       password: password,
     };
 
-    try {
-      const response = await fetch("/api/user/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      const result = await response.json();
-      console.log(result)
-
-      if (result.user_code === 1) {
-        localStorage.setItem('idusuario', result.user_code)
-        setOpenModal(true);
-        setMessage(true)
-        setTimeout(() => {
-          navigate('/Menu');
-          login(result.user_code);
-        }, 2400);
-      } else {
-        setOpenModal(true);
-        setMessage(false);
-        setTimeout(() => {
-          setOpenModal(false);
-        }, 1600);
-      }
-    } catch (error) {
-      console.error("Error en la solicitud:", error);
-      setMessage("Hubo un problema al conectar con el servidor.");
+    if (user=="MartinG" && password=="#7martin7#") {
+      localStorage.setItem('idusuario', user)
+      setOpenModal(true);
+      setMessage(true)
+      setTimeout(() => {
+        navigate('/Menu');
+        login(user);
+      }, 2400);
+    } else {
+      setOpenModal(true);
+      setMessage(false);
+      setTimeout(() => {
+        setOpenModal(false);
+      }, 1600);
     }
   };
 
@@ -68,26 +93,26 @@ const Login = ({ login }) => {
             required
           />
 
-            <label htmlFor="password" className="label">Contraseña</label>
-            <div className="password-container">
-              <input
-                type={showPassword ? 'text' : 'password'}  // Cambiar tipo de input según el estado
-                id="password"
-                className="input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Contraseña"
-                required
-              />
-              <button
-                type="button"
-                className="buttonEye-icon"
-                onClick={() => setShowPassword(!showPassword)}  // Cambiar el estado para mostrar/ocultar
-              >
-                {!showPassword ? <FaEyeSlash /> : <FaEye />}
-              </button>
-            </div>
-       
+          <label htmlFor="password" className="label">Contraseña</label>
+          <div className="password-container">
+            <input
+              type={showPassword ? 'text' : 'password'}  // Cambiar tipo de input según el estado
+              id="password"
+              className="input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Contraseña"
+              required
+            />
+            <button
+              type="button"
+              className="buttonEye-icon"
+              onClick={() => setShowPassword(!showPassword)}  // Cambiar el estado para mostrar/ocultar
+            >
+              {!showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
+
           <button className="buttonIngresar" type="submit">Ingresar</button>
         </form>
       </div>
